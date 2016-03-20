d0b2d2f1145c18aa14f44a4fa0a66d1f35dac3a1
HDxDaniel <illescas.daniel@protonmail.com>
2016-03-20 18:44:29 +0100

    Added files via upload

---
+++ matrix.cpp
@@ -0,0 +1,35 @@
+#include "matrix.h"
+
+template <class T>
+matrix<T>::matrix(unsigned int x, unsigned int y){
+	resize(x,y);
+}
+
+template <class T>
+matrix<T>::matrix(matrix& matrix){
+	*this = matrix;
+}
+
+template <class T>
+void matrix<T>::resize(unsigned int x, unsigned int y){
+	m.resize(x, vector<T>(y));
+}
+
+template <class T>
+vector<T> & matrix<T>::operator[](unsigned int i) {
+	return m[i];
+}
+
+template <class T>
+matrix<T>& matrix<T>::operator=(matrix& matrix){
+	m = matrix.m;
+	return (*this);
+}
+
+//
+
+template class matrix <int>;
+template class matrix <double>;
+template class matrix <float>;
+template class matrix <char>;
+template class matrix <string>;
