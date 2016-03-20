755d43b90430a683e1f918bd3011875004d24e06
HDxDaniel <illescas.daniel@protonmail.com>
2016-03-20 18:57:20 +0100

    Added files via upload

---
+++ Example.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "matrix.h"
+
+using namespace std;
+
+int main(){
+
+	// New matrix, size: 10x10
+	matrix<int> m(10, 10);
+
+	// Inserts a value on the specified position and shows its content
+	m[0][2] = 95;
+	cout << m[0][2] << endl;
+
+	// Makes the matrix bigger
+	m.resize(20, 20);
+
+	// Inserts a new value and shows its value
+	m[11][8] = 100;
+	cout << m[0][2] << ' ' << m[11][8] <<  endl;
+
+	// Copy constructor
+	matrix<int> m2(m);
+	cout << m[11][8] << endl;
+
+
+	//
+	cin.ignore();
+	
+	return 0;
+}
